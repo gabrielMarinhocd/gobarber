50017237ce47af91c7737f31a6971d4eb4e50f80
Gabriel Marinho <biel32771@gmail.com>
2021-01-30 12:00:39 -0300

    Criando tabela de Users e adcionando  a tabela de Appointments metodos CreateDateColumn e UdateDateColumn

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -2,20 +2,20 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
-  UpdateDateColumn,
   CreateDateColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  name: string;
 
-  @Column('timestamp with time zone')
-  date: Date;
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   created_at: Date;
@@ -24,4 +24,4 @@ class Appointment {
   updated_at: Date;
 }
 
-export default Appointment;
+export default User;
