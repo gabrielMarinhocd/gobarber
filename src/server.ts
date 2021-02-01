6f73e347c1328a290b9f545627dc53779383be78
Gabriel Marinho <biel32771@gmail.com>
2021-02-01 15:15:25 -0300

    Adcionando rota get de avatar

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(3333, () => {
   console.log('Api Start');
